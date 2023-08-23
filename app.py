9a2d5c3dd7470e231d874eb3cf0b9117a858997d
Daniel Melichar <daniel@melichar.xyz>
2023-08-23 17:45:19 +0200

    Moved to HTMX

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import datetime
 
 from flask import (
     Flask,
@@ -10,51 +11,71 @@ from flask import (
     url_for,
 )
 
+from flask_htmx import HTMX
+from flask_htmx import make_response
+
 app = Flask(__name__)
+htmx = HTMX(app)
 
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect("database.db")
     conn.row_factory = sqlite3.Row
     return conn
 
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/messages", methods=["GET"])
+def message():
     conn = get_db_connection()
-    people = conn.execute("SELECT * FROM people").fetchall()
+    messages = conn.execute("SELECT * FROM message").fetchall()
     conn.close()
-    print("Request for index page received")
-    return render_template("index.html", people=people)
 
+    tmpl = """
+    <tr>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
+    </tr>
+    """
+    body = [tmpl.format(m["person"], m["text"], m["created"]) for m in messages]
+    resp = "".join(body)
 
-@app.route("/favicon.ico")
-def favicon():
-    return send_from_directory(
-        os.path.join(app.root_path, "static"),
-        "favicon.ico",
-        mimetype="image/vnd.microsoft.icon",
-    )
+    return make_response(resp, push_url=False)
 
 
 @app.route("/hello", methods=["POST"])
 def hello():
     name = request.form.get("name")
+    message = request.form.get("message")
+    timestamp = str(datetime.datetime.now())
 
-    if name:
-        print("Request for hello page received with name=%s" % name)
+    if name and message:
+        print(f"Request for hello page received with name={name} and message={message}")
 
         conn = get_db_connection()
-        people = conn.execute(f"INSERT INTO people (person) VALUES ('{name}')")
+        conn.execute(
+            f"INSERT INTO message (person, text, created) VALUES ('{name}', '{message}', '{timestamp}')"
+        )
         conn.commit()
         conn.close()
 
-        return render_template("hello.html", name=name)
+        resp = f"""
+        <tr>
+            <td>{name}</td>
+            <td>{message}</td>
+            <td>{timestamp}</td>
+        </tr>
+        """
+
+        return make_response(resp, push_url=False)
     else:
-        print(
-            "Request for hello page received with no name or blank name -- redirecting"
-        )
-        return redirect(url_for("index"))
+        print("error")
+        return None
 
 
 if __name__ == "__main__":
--- init_db.py
+++ init_db.py
@@ -1,17 +1,46 @@
 import sqlite3
+from random import randrange
+from datetime import timedelta, datetime
 
-connection = sqlite3.connect('database.db')
+
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
 
 
-with open('db/schema.sql') as f:
+d1 = datetime.strptime("1/1/2022 1:30 PM", "%m/%d/%Y %I:%M %p")
+d2 = datetime.strptime("1/1/2023 4:50 AM", "%m/%d/%Y %I:%M %p")
+
+timestamp1 = random_date(d1, d2)
+timestamp2 = random_date(d1, d2)
+timestamp3 = random_date(d1, d2)
+timestamp4 = random_date(d1, d2)
+
+connection = sqlite3.connect("database.db")
+
+with open("db/schema.sql") as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO people (person) VALUES ('Tom')")
-cur.execute("INSERT INTO people (person) VALUES ('Peter')")
-cur.execute("INSERT INTO people (person) VALUES ('Max')")
-cur.execute("INSERT INTO people (person) VALUES ('Robert')")
+cur.execute(
+    f"INSERT INTO message (person, text, created) VALUES ('Tom', 'Hello, world!', '{timestamp1}')"
+)
+cur.execute(
+    f"INSERT INTO message (person, text, created) VALUES ('Peter', 'I like Pizza', '{timestamp2}')"
+)
+cur.execute(
+    f"INSERT INTO message (person, text, created) VALUES ('Max', 'carpe diem', '{timestamp3}')"
+)
+cur.execute(
+    f"INSERT INTO message (person, text, created) VALUES ('Robert', 'woof', '{timestamp4}')"
+)
 
 connection.commit()
 connection.close()
